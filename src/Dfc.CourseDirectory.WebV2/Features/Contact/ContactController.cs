899acbd8f898bcfe39d9d9549b4b56367231eab3
James Law <james.law@alterius.co.uk>
2020-11-06 13:55:45 +0000

    Added [AllowDeactivatedProvider] to /contact route

--- src/Dfc.CourseDirectory.WebV2/Features/Contact/ContactController.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/Contact/ContactController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dfc.CourseDirectory.WebV2.Filters;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dfc.CourseDirectory.WebV2.Features.Contact
 {
     [Route("contact")]
+    [AllowDeactivatedProvider]
     public class ContactController : Controller
     {
         [HttpGet]
